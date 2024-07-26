fe119ea0303ae23a7b1a186450aad15d252a0c3f
bijaykumarpun <bijay.workplace@gmail.com>
2024-07-26 12:27:55 +0545

    chore(init): create a new project

---
+++ src/main/kotlin/com/global/blog/post/blog_post/BlogPostApplication.kt
@@ -0,0 +1,12 @@
+package com.global.blog.post.blog_post
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+ear
+g
+@SpringBootApplication
+class BlogPostApplication
+
+fun main(args: Array<String>) {
+	runApplication<BlogPostApplication>(*args)
+}
---
+++ src/test/kotlin/com/global/blog/post/blog_post/BlogPostApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.global.blog.post.blog_post
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogPostApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
